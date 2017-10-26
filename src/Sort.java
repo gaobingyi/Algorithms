e46098fa6148e2bac6acfcc1bcb9a61eece95ff7
gaobingyi <bingyi.gao@foxmail.com>
2017-10-27 00:22:22 +0800

    add some sort algorithms

--- src/Sort.java
+++ src/Sort.java
@@ -1,11 +1,77 @@
-
 public class Sort {
 
 	public static void main(String[] args) {
-		int[] array = {2,1,3,-1,4,3};
+		int[] array = null;
+		// Bubble Sort
+		array = new int[] {2,1,3,-1,4,3};
+		bubbleSort(array);
+		Utils.printArr(array);
+		// Select Sort
+		array = new int[] {2,1,3,-1,4,3};
+		selectSort(array);
+		Utils.printArr(array);
+		// Insert Sort
+		array = new int[] {2,1,3,-1,4,3};
+		insertSort(array);
+		Utils.printArr(array);
 		// Quick Sort
+		array = new int[] {2,1,3,-1,4,3};
 		quickSort(array);
 		Utils.printArr(array);
+	}
+	
+	/*
+	 * Bubble Sort
+	 */
+	public static void bubbleSort(int[] array) {
+		for (int i = 0; i < array.length - 1; i++) {
+			boolean flag = true;
+			for (int j = 0; j < array.length - 1 - i; j++) {
+				if (array[j] > array[j+1]) {
+					swap(array, j, j+1);
+					flag = false;
+				}
+			}
+			if (flag) {
+				break;
+			}
+		}
+	}
+	
+	/*
+	 * Select Sort
+	 */
+	public static void selectSort(int[] array) {
+		for (int i = 0; i < array.length - 1; i++) {
+			int minIndex = i; // 假设第一个元素最小
+			for (int j = i+1; j < array.length; j++) {
+				if (array[j] < array[minIndex]) {
+					minIndex = j;
+				}
+			}
+			if (minIndex != i) {
+				swap(array, minIndex, i);
+			}
+		}
+	}
+	
+	/*
+	 * Insert Sort
+	 */
+	public static void insertSort(int[] array) {
+		for (int i = 1; i < array.length; i++) {
+			int j = i;
+			while (j > 0 && array[j] < array[j-1]) {
+				swap(array, j, j-1);
+				j--;
+			}
+		}
+	}
+	
+	private static void swap(int[] array, int s, int t) {
+		int temp = array[s];
+		array[s] = array[t];
+		array[t] = temp;
 	}
 	
 	/*
