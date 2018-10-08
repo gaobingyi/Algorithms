d5031a3a2418e9287a72cb8a921900d70089ca6e
gaobingyi <bingyi.gao@foxmail.com>
2018-10-09 07:02:20 +0800

    modify code

--- src/Combination.java
+++ src/Combination.java
@@ -1,32 +1,58 @@
+
+
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 全组合
+ * @author Administrator
+ *
+ */
 public class Combination {
-
 	public static void main(String[] args) {
-		combination("abc");
-	}
-	
-	public static void combination(String s) {
-		if (s == null)
-			return;
-		List<Character> list = new ArrayList<Character>();
-		for (int i = 1; i <= s.length(); i++) {
-			combination(s, i, list);
-		}
-	}
-	
-	public static void combination(String s, int n, List<Character> list) {
-		if (n == 0) {
+		char[] chs = {'a', 'b', 'c'};
+		List<List<Character>> resultList = combination(chs);
+		for (List<Character> list : resultList) {
 			System.out.println(list);
+		}
+	}
+	
+	/**
+	 * 获取字符数组的全组合（即从m个元素中选择n（0<n<=m）个元素的所有组合）
+	 * @param chs 字符数组
+	 * @return
+	 */
+	public static List<List<Character>> combination(char[] chs) {
+		if (chs == null || chs.length == 0)
+			return new ArrayList<>();
+		List<List<Character>> resultList = new ArrayList<>();
+		List<Character> tempList = new ArrayList<Character>();
+		for (int i = 1; i <= chs.length; i++) {
+			combination(chs, 0, i, tempList, resultList);
+		}
+		return resultList;
+	}
+	
+	/**
+	 * 获取从chs.length个字符中选择size个元素的所有组合
+	 * @param chs 字符数组
+	 * @param start 选择范围的起始位置
+	 * @param size 选择的元素个数
+	 * @param tempList 存放某一种组合
+	 * @param resultList 存放所有组合
+	 */
+	public static void combination(char[] chs, int start, int size, List<Character> tempList, List<List<Character>> resultList) {
+		if (size == 0) {
+			resultList.add(new ArrayList<Character>(tempList));
 			return;
 		}
-		if (s.length() == 0)
+		if (start == chs.length)
 			return;		
-		list.add(s.charAt(0));
-		combination(s.substring(1), n-1, list);
-		list.remove(list.size() - 1);
-		combination(s.substring(1), n, list);
+		// 假设选中第一个元素，则从后面的子串中选择size-1个
+		tempList.add(chs[start]);
+		combination(chs, start+1, size-1, tempList, resultList);
+		// 假设不选第一个元素，则从后面的子串中选择size个
+		tempList.remove(tempList.size() - 1);
+		combination(chs, start+1, size, tempList, resultList);
 	}
-
 }
--- src/Permutation.java
+++ src/Permutation.java
@@ -1,40 +1,82 @@
-public class Permutation {
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 全排列
+ * @author Administrator
+ *
+ */
+public class Permutation {
 	public static void main(String[] args) {
-		permutation("abc");
-	}
-	
-	public static void permutation(String s) {
-		char[] array = s.toCharArray();
-		permutation(array, 0);
-	}
-	
-	private static void permutation(char[] array, int begin) {
-		if (begin == array.length - 1) {
-			System.out.println(array);
+		String str = "abc";
+		List<String> resultList = permutation(str);
+		for (String string : resultList) {
+			System.out.println(string);
 		}
-		for (int i = begin; i < array.length; i++) {
-			if (isSwap(array, begin, i)) {
-				swap(array, begin, i);
-				permutation(array, begin+1);
-				swap(array, begin, i);
+	}
+	
+	/**
+	 * 获取字符串的全排列
+	 * @param str
+	 * @return 
+	 */
+	public static List<String> permutation(String str) {
+		char[] chs = str.toCharArray();
+		List<String> resultList = new ArrayList<>();
+		permutation(chs, 0, resultList);
+		return resultList;
+	}
+	
+	/**
+	 * 
+	 * @param chs
+	 * @param beginPos
+	 */
+	private static void permutation(char[] chs, int beginPos, List<String> resultList) {
+		if (beginPos == chs.length - 1) {
+			resultList.add(new String(chs));
+		}
+		for (int i = beginPos; i < chs.length; i++) {
+			if (isSwap(chs, beginPos, i)) {
+				swap(chs, beginPos, i);
+				permutation(chs, beginPos + 1, resultList);
+				swap(chs, beginPos, i);
 			}
 		}
 	}
 	
-	private static boolean isSwap(char[] array, int s, int t) {
-		for (int i = s; i < t; i++) {
-			if (array[i] == array[t]) {
+	/**
+	 * 功能：判断是否应该交换元素，从而去除重复的排列情况
+	 * 若在当前字符之前的元素中存在当前字符，则不交换。
+	 * @param chs
+	 * @param beginPos
+	 * @param curPos
+	 * @return
+	 */
+	private static boolean isSwap(char[] chs, int beginPos, int curPos) {
+		for (int i = beginPos; i < curPos; i++) {
+			if (chs[i] == chs[curPos]) {
 				return false;
 			}
 		}
 		return true;
 	}
- 
-	private static void swap(char[] array, int s, int t) {
-		char temp = array[s];
-		array[s] = array[t];
-		array[t] = temp;
+	
+	/**
+	 * 交换数组中两个位置的元素
+	 * @param chs
+	 * @param pos1
+	 * @param pos2
+	 */
+	private static void swap(char[] chs, int pos1, int pos2) {
+		if (pos1 == pos2) {
+			return;
+		}
+		char temp = chs[pos1];
+		chs[pos1] = chs[pos2];
+		chs[pos2] = temp;
 	}
 
 }
