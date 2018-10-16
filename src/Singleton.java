6bc4912160a025b6639cce29594d2bdd19913250
gaobingyi <bingyi.gao@foxmail.com>
2018-10-17 07:06:47 +0800

    add Singleton.java

---
+++ src/Singleton.java
@@ -0,0 +1,37 @@
+
+
+public class Singleton {
+	// 1.饿汉式
+//	private static Singleton instance = new Singleton();
+//	private Singleton() {}
+//	public static Singleton getInstance() {
+//		return instance;
+//	}
+	// 2.懒汉式（双重检验锁）
+//	private static volatile Singleton instance;
+//	private Singleton() {}
+//	public static Singleton getInstance() {
+//		if (instance == null) {
+//			synchronized (Singleton.class) {
+//				if (instance == null) {
+//					instance = new Singleton();
+//				}
+//			}
+//		}
+//		return instance;
+//	}
+	// 3.静态内部类
+	private Singleton() {}
+	private static class StaticInner {
+		private static final Singleton instance = new Singleton();
+	}
+	public static Singleton getInstance() {
+		return StaticInner.instance;
+	}
+	
+}
+
+// 4.枚举
+//public enum Singleton {
+//	INSTANCE;
+//}
